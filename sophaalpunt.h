0b903236388dbe37be7661c4fd2926c2ffe296e9
Wim Deprez <wim@tamale.nl>
2014-07-23 20:06:26 +0200

    New Enum "SOphaalpunt"
    Seems useful

---
+++ sophaalpunt.h
@@ -0,0 +1,39 @@
+#ifndef SOPHAALPUNT_H
+#define SOPHAALPUNT_H
+
+#include <QSettings>
+
+extern QSettings settings;
+
+
+//structure for save markers data
+struct SOphaalpunt
+{
+    SOphaalpunt()
+    {
+        naam = "";
+        kg_kurk = 0;
+        kg_kaarsresten = 0;
+        zakken_kurk = 0;
+        zakken_kaarsresten = 0;
+        adres = "";
+    };
+    SOphaalpunt(QString _naam, double _kg_kurk, double _kg_kaarsresten, double _zakken_kurk, double _zakken_kaarsresten, QString _adres)
+    {
+        naam = _naam;
+        kg_kurk = _kg_kurk;
+        kg_kaarsresten = _kg_kaarsresten;
+        zakken_kurk = _zakken_kurk;
+        zakken_kaarsresten = _zakken_kaarsresten;
+        adres = _adres;
+    };
+
+    QString naam;
+    double kg_kurk;
+    double kg_kaarsresten;
+    double zakken_kurk;
+    double zakken_kaarsresten;
+    QString adres;
+};
+
+#endif // SOPHAALPUNT_H
