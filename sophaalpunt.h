5fd5d6fdd666764eb529f91e639ca757c180d9af
Wim Deprez <wim@tamale.nl>
2014-07-28 15:03:34 +0200

    Expanding SOphaalpunt

--- sophaalpunt.h
+++ sophaalpunt.h
@@ -17,8 +17,10 @@ struct SOphaalpunt
         zakken_kurk = 0;
         zakken_kaarsresten = 0;
         adres = "";
+        id = -1;
     };
-    SOphaalpunt(QString _naam, double _kg_kurk, double _kg_kaarsresten, double _zakken_kurk, double _zakken_kaarsresten, QString _adres)
+
+    SOphaalpunt(QString _naam, double _kg_kurk, double _kg_kaarsresten, double _zakken_kurk, double _zakken_kaarsresten, QString _adres, int _id)
     {
         naam = _naam;
         kg_kurk = _kg_kurk;
@@ -26,7 +28,19 @@ struct SOphaalpunt
         zakken_kurk = _zakken_kurk;
         zakken_kaarsresten = _zakken_kaarsresten;
         adres = _adres;
+        id = _id;
     };
+
+    double getVolume()
+    {
+        double volume = (zakken_kurk * settings.value("zak_kurk_volume").toDouble()) + (zakken_kaarsresten * settings.value("zak_kaarsresten_volume").toDouble());
+        return volume;
+    }
+
+    double getWeight()
+    {
+        return kg_kurk + kg_kaarsresten;
+    }
 
     QString naam;
     double kg_kurk;
@@ -34,6 +48,7 @@ struct SOphaalpunt
     double zakken_kurk;
     double zakken_kaarsresten;
     QString adres;
+    int id;
 };
 
 #endif // SOPHAALPUNT_H
