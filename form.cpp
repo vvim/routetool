e3a88bceeb7968fbbf539af73ed36b6a61c5ae67
Wim Deprez <wim@tamale.nl>
2014-08-08 15:26:44 +0200

    Added Javascript-code from Lemonharpy
    Google API doesn't allow more than 8 WayPoints
    the workaround used here is courtesy of 'lemonharpy':
     * http://lemonharpy.wordpress.com/2011/12/15/working-around-8-waypoint-
     * http://jsfiddle.net/ZyHnk/

--- form.cpp
+++ form.cpp
@@ -562,57 +562,42 @@ void Form::keyPressEvent( QKeyEvent *k )
 
 void Form::drawRoute()
 {
-    //reorder Markers:: see BUG
-          // <vvim> BUG: er is geen SIGNAL voor Drag/Drop vanuit QListWidget. Het programma weet dus niet
-          //             of er markers van plaats zijn veranderd. Zeer vervelend.
-          //             Bestaat er een extensie voor Qt die deze SIGNAL wel heeft?
-    reorderMarkers();
-
-    qDebug() << "<vvim>: Google MAX is 8 WayPoints. To beat this, see http://lemonharpy.wordpress.com/2011/12/15/working-around-8-waypoint-limit-in-google-maps-directions-api/ and http://jsfiddle.net/ZyHnk/";
-
-    //info: https://www.youtube.com/watch?v=nN85QMYZzQQ
-
-
     // this only makes sense when there is more than 1 marker in m_markers
     // (as the first marker is the STARTING POINT and DESTINATION POINT of the route)
     if(m_markers.length() > 1)
     {
-        QString html = QString("<!DOCTYPE html> <html> <head> <meta name=\"viewport\" content=\"initial-scale=1.0, user-scalable=no\" /> <style type=\"text/css\"> html { height: 100% } body { height: 100%; margin: 0; padding: 0 } #map_canvas { height: 100% } </style> <script type=\"text/javascript\" src=\"http://maps.googleapis.com/maps/api/js?key=")+
+        //reorder Markers:: see BUG
+              // <vvim> BUG: er is geen SIGNAL voor Drag/Drop vanuit QListWidget. Het programma weet dus niet
+              //             of er markers van plaats zijn veranderd. Zeer vervelend.
+              //             Bestaat er een extensie voor Qt die deze SIGNAL wel heeft?
+        reorderMarkers();
+
+        /**  ***********************************************  **
+             Google API doesn't allow more than 8 WayPoints
+             the workaround used here is courtesy of 'lemonharpy':
+                   * http://lemonharpy.wordpress.com/2011/12/15/working-around-8-waypoint-limit-in-google-maps-directions-api/
+                   * http://jsfiddle.net/ZyHnk/
+         **  ***********************************************  **/
+
+        QString html_top = QString("<!DOCTYPE html> <html> <head> <meta name=\"viewport\" content=\"initial-scale=1.0, user-scalable=no\" /> <style type=\"text/css\"> html { height: 100% } body { height: 100%; margin: 0; padding: 0 } #map_canvas { height: 100% } </style> <script type=\"text/javascript\" src=\"http://maps.googleapis.com/maps/api/js?key=")+
                 settings.value("apiKey").toString()+
-                QString("&sensor=false\"> </script> <script type=\"text/javascript\"> var map; var markers = []; var directionDisplay; var directionsService = new google.maps.DirectionsService(); var waypts = []; function initialize() { var myOptions = { center: new google.maps.LatLng(50.9801, 4.97517), zoom: 15, mapTypeId: google.maps.MapTypeId.ROADMAP, panControl: true }; map = new google.maps.Map(document.getElementById(\"map_canvas\"), myOptions); directionsDisplay = new google.maps.DirectionsRenderer(); directionsDisplay.setMap(map); var request = { origin:\"")+
-                m_markers.first()->caption + //settings.value("startpunt").toString()+
-                QString("\", destination:\"")+
-                m_markers.first()->caption + //settings.value("startpunt").toString()+
-                QString("\", waypoints: waypts, travelMode: google.maps.DirectionsTravelMode.DRIVING, avoidHighways: true }; directionsService.route(request, function(response, status) { if (status == google.maps.DirectionsStatus.OK) { directionsDisplay.setDirections(response); } }); ");
+                QString("&sensor=false\"> </script> <script type=\"text/javascript\"> var markers = []; var map; function Tour_startUp(stops) { if (!window.tour) window.tour = { updateStops: function (newStops) { stops = newStops; }, loadMap: function (map, directionsDisplay) { var myOptions = { zoom: 15, center: new window.google.maps.LatLng(50.9801, 4.97517), mapTypeId: window.google.maps.MapTypeId.ROADMAP }; map.setOptions(myOptions); directionsDisplay.setMap(map); }, fitBounds: function (map) { var bounds = new window.google.maps.LatLngBounds(); jQuery.each(stops, function (key, val) { var myLatlng = new window.google.maps.LatLng(val.Geometry.Latitude, val.Geometry.Longitude); bounds.extend(myLatlng); }); map.fitBounds(bounds); }, calcRoute: function (directionsService, directionsDisplay) { var batches = []; var itemsPerBatch = 10; var itemsCounter = 0; var wayptsExist = stops.length > 0; while (wayptsExist) { var subBatch = []; var subitemsCounter = 0; for (var j = itemsCounter; j < stops.length; j++) { subitemsCounter++; subBatch.push({ location: new window.google.maps.LatLng(stops[j].Geometry.Latitude, stops[j].Geometry.Longitude), stopover: true }); if (subitemsCounter == itemsPerBatch) break; } itemsCounter += subitemsCounter; batches.push(subBatch); wayptsExist = itemsCounter < stops.length; itemsCounter--; } var combinedResults; var unsortedResults = [{}]; var directionsResultsReturned = 0; for (var k = 0; k < batches.length; k++) { var lastIndex = batches[k].length - 1; var start = batches[k][0].location; var end = batches[k][lastIndex].location; var waypts = []; waypts = batches[k]; waypts.splice(0, 1); waypts.splice(waypts.length - 1, 1); var request = { origin: start, destination: end, waypoints: waypts, travelMode: window.google.maps.TravelMode.WALKING }; (function (kk) { directionsService.route(request, function (result, status) { if (status == window.google.maps.DirectionsStatus.OK) { var unsortedResult = { order: kk, result: result }; unsortedResults.push(unsortedResult); directionsResultsReturned++; if (directionsResultsReturned == batches.length) { unsortedResults.sort(function (a, b) { return parseFloat(a.order) - parseFloat(b.order); }); var count = 0; for (var key in unsortedResults) { if (unsortedResults[key].result != null) { if (unsortedResults.hasOwnProperty(key)) { if (count == 0) combinedResults = unsortedResults[key].result; else { combinedResults.routes[0].legs = combinedResults.routes[0].legs.concat(unsortedResults[key].result.routes[0].legs); combinedResults.routes[0].overview_path = combinedResults.routes[0].overview_path.concat(unsortedResults[key].result.routes[0].overview_path); combinedResults.routes[0].bounds = combinedResults.routes[0].bounds.extend(unsortedResults[key].result.routes[0].bounds.getNorthEast()); combinedResults.routes[0].bounds = combinedResults.routes[0].bounds.extend(unsortedResults[key].result.routes[0].bounds.getSouthWest()); } count++; } } } directionsDisplay.setDirections(combinedResults); } } }); })(k); } } }; } function initialize() { var stops = [");
 
-        /*  --> change HTML to forget the previous markers, but the put the route instead.
-                Then new markers will be added as "markers", but not yet in the route.
+        QString html_bottom = QString("] ; var myOptions = { center: new google.maps.LatLng(50.9801, 4.97517), zoom: 15, mapTypeId: google.maps.MapTypeId.ROADMAP, panControl: true }; map = new window.google.maps.Map(document.getElementById(\"map_canvas\"), myOptions); var directionsDisplay = new window.google.maps.DirectionsRenderer(); var directionsService = new window.google.maps.DirectionsService(); Tour_startUp(stops); window.tour.loadMap(map, directionsDisplay); if (stops.length > 1) window.tour.calcRoute(directionsService, directionsDisplay); }; </script> </head> <body onLoad='initialize()'> <div id=\"map_canvas\" style=\"width:100%;height:100%\"> </div> </body> </html>");
 
-                What will happen when we try to remove markers that are already in the route? Error?
-        */
-
-        QList <SMarker*> m_markers_minus_startingpoint = m_markers;
-        m_markers_minus_startingpoint.removeFirst();
-
-        foreach(SMarker* m, m_markers_minus_startingpoint)
+        foreach(SMarker* marker, m_markers)
         {
-            QString str =
-                    QString("waypts.push({") +
-                    QString("location: new google.maps.LatLng(%1,%2),").arg(m->north).arg(m->east) +
-//                    QString("location: \"%1\",").arg(m->caption) +
-                    QString("stopover: true") +
-                    QString("});");
-
-            qDebug() << str;
-            html += str;
+            QString str = QString("{\"Geometry\":{\"Latitude\": %1 ,\"Longitude\": %2 }},\n").arg(marker->north).arg(marker->east);
+            html_top += str;
         }
 
-        html += QString("} </script> </head> <body onload=\"initialize()\"> <div id=\"map_canvas\" style=\"width:100%;height:100%\"> </div> </body> </html> ");
+        // return again to starting point:
+        QString str = QString("{\"Geometry\":{\"Latitude\": %1 ,\"Longitude\": %2 }}\n").arg(m_markers[0]->north).arg(m_markers[0]->east);
+        html_top += str;
 
-        ui->webView->setHtml(html);
+        qDebug() << "complete HTML:" << html_top+html_bottom;
+        ui->webView->setHtml(html_top+html_bottom);
     }
-
-
 }
 
 void Form::on_pbRouteOmdraaien_clicked()
