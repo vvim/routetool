80e0215dcb8088c7cc52b52eb47b7142c45d36c7
Wim Deprez <wim@tamale.nl>
2014-08-13 17:34:04 +0200

    Small change in MySortFilterProxyModel()

--- mysortfilterproxymodel.cpp
+++ mysortfilterproxymodel.cpp
@@ -8,6 +8,9 @@ MySortFilterProxyModel::MySortFilterProxyModel(QObject *parent)
 
 bool MySortFilterProxyModel::lessThan(const QModelIndex &left, const QModelIndex &right) const
 {
+    // Qt::DisplayRole nog aanpassen voor QDate?
+    // zie eerste vb van modelview
+
     QVariant leftData = sourceModel()->data(left);
     QVariant rightData = sourceModel()->data(right);
 
@@ -18,6 +21,10 @@ bool MySortFilterProxyModel::lessThan(const QModelIndex &left, const QModelIndex
     else if (leftData.type() == QVariant::Int)
     {
         return leftData.toInt() < rightData.toInt();
+    }
+    else if (leftData.type() == QVariant::Double)
+    {
+        return leftData.toDouble() < rightData.toDouble();
     }
     else
     {
