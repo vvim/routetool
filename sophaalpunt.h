a99e0ceaf7162af9225a3dc7539adec574f273e3
Wim Deprez <wim@tamale.nl>
2015-10-09 15:16:49 +0200

    Edit of SOphaalpunt to contain latitude/longitude

--- sophaalpunt.h
+++ sophaalpunt.h
@@ -26,9 +26,11 @@ struct SOphaalpunt
         aanmelding_id = -1;
         ophaalpunt_id = -1;
         opmerkingen = "";
+        lat = 0.0;
+        lng = 0.0;
     };
 
-    SOphaalpunt(QString _naam, QString _street, QString _housenr, QString _busnr, QString _postalcode, QString _plaats, QString _country, double _kg_kurk, double _kg_kaarsresten, double _zakken_kurk, double _zakken_kaarsresten, int _aanmelding_id, int _ophaalpunt_id, QString _opmerkingen)
+    SOphaalpunt(QString _naam, QString _street, QString _housenr, QString _busnr, QString _postalcode, QString _plaats, QString _country, double _kg_kurk, double _kg_kaarsresten, double _zakken_kurk, double _zakken_kaarsresten, int _aanmelding_id, int _ophaalpunt_id, QString _opmerkingen, double _lat = 0.0, double _lng = 0.0)
     {
         naam = _naam;
         kg_kurk = _kg_kurk;
@@ -44,6 +46,8 @@ struct SOphaalpunt
         aanmelding_id = _aanmelding_id;
         ophaalpunt_id = _ophaalpunt_id;
         opmerkingen = _opmerkingen;
+        lat = _lat;
+        lng = _lng;
     };
 
     double getVolume()
@@ -106,6 +110,15 @@ struct SOphaalpunt
         return ophaalpunt_id;
     }
 
+    double getLatitude()
+    {
+        return lat;
+    }
+
+    double getLongitude()
+    {
+        return lng;
+    }
 
     QString naam;
     double kg_kurk;
@@ -121,6 +134,8 @@ struct SOphaalpunt
     int aanmelding_id;
     int ophaalpunt_id;
     QString opmerkingen;
+    double lat;
+    double lng;
 };
 
 #endif // SOPHAALPUNT_H
